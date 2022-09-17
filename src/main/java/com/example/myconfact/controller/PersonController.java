0e4a7f4abbb928910089b257aed2a07e84f1245a
grace <sg32a2@naver.com>
2022-09-17 22:54:21 +0900

    Controller Test

--- src/main/java/com/example/myconfact/controller/PersonController.java
+++ src/main/java/com/example/myconfact/controller/PersonController.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
 @RequestMapping(value = "/api/person")
 @RestController
 @Slf4j
